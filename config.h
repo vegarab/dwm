7e1ac6a3fee0c3cbcb464b8ce65fd5e40049ae15
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2020-07-12 00:17:57 +0200

    patch: systray

--- config.h
+++ config.h
@@ -11,6 +11,10 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const char *fonts[]          = { "monospace:size=15", "JoyPixels:pixelsize=15:antialias=true:autohint=true"  };
 static char dmenufont[]             = "monospace:size=15";
 static char normbgcolor[]           = "#282828";
